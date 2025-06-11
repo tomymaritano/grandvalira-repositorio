536688f6d12f12e702e9d174c9a6e916a0876a1a
Tomás Maritano <tomymaritano@gmail.com>
2025-06-11 04:29:28 -0300

    Remove unused audit service import

--- backend/src/services/contacts.service.js
+++ backend/src/services/contacts.service.js
@@ -1,18 +1,17 @@
-const contactRepository = require('../repositories/contact.repository');
-const auditService = require('./audit.service');
+const contactRepository = require('../repositories/contact.repository')
 
 exports.getContacts = async (options) => {
-  return await contactRepository.getContacts(options);
-};
+  return await contactRepository.getContacts(options)
+}
 
 exports.createContact = async (data) => {
-  return await contactRepository.createContact(data);
-};
+  return await contactRepository.createContact(data)
+}
 
 exports.updateContact = async (id, data) => {
-  return await contactRepository.updateContact(id, data);
-};
+  return await contactRepository.updateContact(id, data)
+}
 
 exports.banContact = async (id) => {
-  return await contactRepository.banContact(id);
-};
+  return await contactRepository.banContact(id)
+}
