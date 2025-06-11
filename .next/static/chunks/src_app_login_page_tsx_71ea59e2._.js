5ac9a38be423fc00b6980ac4a904f931fb96764c
tomymaritano <tomymaritano@gmail.com>
2025-06-11 03:48:49 -0300

    global changes

--- .next/static/chunks/src_app_login_page_tsx_71ea59e2._.js
+++ .next/static/chunks/src_app_login_page_tsx_71ea59e2._.js
@@ -1,7 +1,8 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
     script: typeof document === "object" ? document.currentScript : undefined,
     chunks: [
-  "static/chunks/_5e3ec0a9._.js"
+  "static/chunks/node_modules_4db5179b._.js",
+  "static/chunks/src_app_1a3fa787._.js"
 ],
     source: "dynamic"
 });
